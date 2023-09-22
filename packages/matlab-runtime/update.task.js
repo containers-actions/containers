66e298e9fbcbf2978aa7373e0e33b2294f2243fa
ZhengJin <fangzhengjin@gmail.com>
2023-09-22 18:49:02 +0800

    fix: rebuild 9.13

--- packages/matlab-runtime/update.task.js
+++ packages/matlab-runtime/update.task.js
@@ -29,6 +29,16 @@ module.exports = async ({
     return ldLibraryPaths.join(':');
   };
 
+  const getInstallParam = (version) => {
+    const installParams = [];
+    installParams.push('-agreeToLicense yes');
+    installParams.push('-destinationFolder /MATLAB_Runtime');
+    if (semver.lte(semver.coerce(version), semver.coerce('9.12'), true)) {
+      installParams.push('-mode silent');
+    }
+    return installParams.join(' ');
+  };
+
   for (const match of html.matchAll(/<td>(?<v1>\w\d{4}\w)\s+\((?<v2>[\d.]+)\)(?:<br>)?<\/td>/gm)) {
     const { v1, v2 } = match.groups;
     if (semver.gte(semver.coerce(v2), semver.coerce('9.11'), true)) {
@@ -42,6 +52,7 @@ module.exports = async ({
         v1: version.split('.')[0], // 2023
         v2, // 9.11
         v3, // 0
+        installParam: getInstallParam(v2),
         ldLibraryPath: getLdLibraryPath(v2.replace('.', '')),
       };
     }
@@ -54,6 +65,7 @@ module.exports = async ({
   if (sortedIndex != 0) {
     const latestVersion = sortedVersions[sortedIndex - 1];
     dockerfile = runtime.replaceVariable('MATLAB_RUNTIME_VERSION', latestVersion, dockerfile);
+    dockerfile = runtime.replaceVariable('MATLAB_RUNTIME_INSTALL_PARAMS', versions[latestVersion].url, dockerfile);
     dockerfile = runtime.replaceVariable('MATLAB_RUNTIME_DOWNLOAD_URL', versions[latestVersion].url, dockerfile);
     dockerfile = runtime.replaceVariable('LD_LIBRARY_PATH', versions[latestVersion].ldLibraryPath, dockerfile);
     await runtime.uploadFileAndCreatePullRequest(package, latestVersion, `${path}/Dockerfile`, dockerfile);
