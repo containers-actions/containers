3d5b9dec35ee930733388da03abed43e87111b75
ZhengJin <fangzhengjin@gmail.com>
2024-10-24 10:40:05 +0800

    feat: libreoffice-nogui

---
+++ packages/libreoffice-nogui/update.task.js
@@ -0,0 +1,24 @@
+module.exports = async ({
+  constants: { package },
+  scripts: { github, context, core, glob, io, exec, fetch, require },
+  runtime,
+}) => {
+  const semver = require('semver');
+  const latestVersion = await runtime.latestDebianPackageVersion('libreoffice-nogui');
+
+  if (latestVersion === '') return null;
+
+  let dockerfile = runtime.readDockerfile(package);
+  const currentVersion = runtime.getVersion('LIBREOFFICE_VERSION', dockerfile);
+
+  if (latestVersion != currentVersion) {
+    dockerfile = runtime.replaceVariable('LIBREOFFICE_VERSION', latestVersion, dockerfile);
+    const cleanedVersion = runtime.debianPackageVersionClean(latestVersion);
+    await runtime.updateFileAndCreatePullRequest(package, cleanedVersion, {
+      Dockerfile: dockerfile,
+      'tags.yml': runtime.dumpImageTags([semver.clean(cleanedVersion, { loose: true }), 'latest']),
+    });
+    return latestVersion;
+  }
+  return null;
+};
