0074f92cbb310b6652f9e366e23c830429452a55
ZhengJin <fangzhengjin@gmail.com>
2024-10-30 10:11:56 +0000

    feat: add cnpmcore

---
+++ packages/cnpmcore/update.task.js
@@ -0,0 +1,19 @@
+module.exports = async ({
+  constants: { package },
+  scripts: { github, context, core, glob, io, exec, fetch, require },
+  runtime,
+}) => {
+  const semver = require('semver');
+  let dockerfile = runtime.readDockerfile(package);
+  const currentVersion = runtime.getDockerfileEnvVersion('CNPMCORE_VERSION', dockerfile);
+  const latestVersion = await runtime.getLatestReleaseTagName({ owner: 'cnpm', repo: 'cnpmcore' });
+  if (semver.lt(currentVersion, latestVersion)) {
+    dockerfile = runtime.replaceVariable('CNPMCORE_VERSION', latestVersion, dockerfile);
+    await runtime.updateFileAndCreatePullRequest(package, latestVersion, {
+      Dockerfile: dockerfile,
+      'tags.yml': runtime.dumpImageTags([semver.clean(latestVersion, { loose: true }), 'latest']),
+    });
+    return latestVersion;
+  }
+  return null;
+};
